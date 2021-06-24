872474b62978c98f9f127f5b78c3e9f59337e88f
sk22 <sk22@mailbox.org>
2021-06-24 12:06:58 +0200

    handle edge case where zip is too big

--- src/components/tweets-loader.js
+++ src/components/tweets-loader.js
@@ -1,7 +1,17 @@
 import JSZip from 'jszip'
-import React, { createRef } from 'react'
+import React, { createRef, useState } from 'react'
+import styled from 'styled-components'
+
+const ErrorOutput = styled.p`
+  color: red;
+`
+
+const FileInput = styled.input`
+  width: 100%;
+`
 
 const TweetsLoader = ({ setTweets }) => {
+  const [error, setError] = useState(null)
   const inputRef = createRef()
 
   const handleTweets = ({ tweet }) => {
@@ -20,6 +30,7 @@ const TweetsLoader = ({ setTweets }) => {
   }
 
   const handleFileChange = async () => {
+    setError(null)
     if (inputRef.current) {
       const file = inputRef.current.files[0]
       if (file && file.name.endsWith('.js')) {
@@ -32,17 +43,34 @@ const TweetsLoader = ({ setTweets }) => {
           console.error('error reading file :/')
         })
       } else if (file && file.name.endsWith('.zip')) {
-        const zip = await JSZip.loadAsync(file)
-        const files = zip.file(/data\/tweet.js/)
-        const tweetJs = await files[0].async('string')
-        handleTweetJs(tweetJs)
+        try {
+          const zip = await JSZip.loadAsync(file)
+          const files = zip.file(/data\/tweet.js/)
+          const tweetJs = await files[0].async('string')
+          handleTweetJs(tweetJs)
+        } catch (err) {
+          setError('zip')
+          console.error(err)
+        }
       }
     }
   }
 
+  const errorMessages = {
+    zip: () => (
+      <>
+        Could not read the ZIP archive or its contained <code>tweet.js</code>{' '}
+        file. The ZIP file might be too big – maybe try manually extracting the
+        tweet.js file from the archive ZIP's <code>data</code> subdirectory and
+        using it directly.
+      </>
+    )
+  }
+
   return (
     <>
-      <input type="file" ref={inputRef} onChange={handleFileChange} />
+      <FileInput type="file" ref={inputRef} onChange={handleFileChange} />
+      {error && <ErrorOutput>{errorMessages[error]()}</ErrorOutput>}
     </>
   )
 }
